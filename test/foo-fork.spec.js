623ec79ca6c1cb43b0e9af614957d51a07d83f23
Christopher Hiller <boneskull@boneskull.com>
2020-10-20 15:56:37 -0700

    fix paths

--- test/foo-fork.spec.js
+++ test/foo-fork.spec.js
@@ -1,19 +1,19 @@
 "use strict";
 
-const {fork} = require('child_process');
+const { fork } = require("child_process");
 const assert = require("assert");
 
-const proc = fork(require.resolve('./src/foo-fork'), {stdio: 'inherit'});
+const proc = fork(require.resolve("../src/foo-fork"), { stdio: "inherit" });
 
-proc.on('message', value => {
-  console.error('received "%s" from subprocess', value);
-  assert.strictEqual('polo', value);
+proc
+  .on("message", (value) => {
+    console.error('received "%s" from subprocess', value);
+    assert.strictEqual("polo", value);
 
-  proc.kill();
-}).on('exit', () => {
-  console.error('subprocess exited; done')
-});
+    proc.kill();
+  })
+  .on("exit", () => {
+    console.error("subprocess exited; done");
+  });
 
-proc.send('marco');
-
-
+proc.send("marco");
--- test/foo-workerpool.spec.js
+++ test/foo-workerpool.spec.js
@@ -3,15 +3,16 @@
 const workerpool = require("workerpool");
 const assert = require("assert");
 
-(async function() {
-
-  const pool = workerpool.pool(require.resolve("./src/foo-workerpool"), {workerType:'process'});
+(async function () {
+  const pool = workerpool.pool(require.resolve("../src/foo-workerpool"), {
+    workerType: "process",
+  });
 
   const result = await pool.exec("foo");
 
   assert.strictEqual(result, "foo");
 
-  console.error('done');
+  console.error("done");
 
   return pool.terminate();
 })();
